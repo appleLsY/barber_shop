fc1dc2c00db7730d322e2f0d56f45b607ef8f8a9
liushuyu <365889074@qq.com>
2017-05-16 22:14:37 +0800

    login ok

--- js/index.js
+++ js/index.js
@@ -3,6 +3,25 @@ var vm=new Vue({
     data:{
         login_in:true,
         if_login:false,
-        name:"pangzi"
-    }
+        name:getCookie("account"),
+    },
+    methods:{
+        getCookie:function(name){
+           var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+           if(arr=document.cookie.match(reg))
+               return unescape(arr[2]);
+           else
+               return null;      
+        }
+    },
+    // created:function(){
+    //     alert("sadas");
+    //     this.name=this.getCookie(name);
+    //     alert(this.name);
+    //     if(this.name!="")
+    //     {
+    //         this.if_login=true;
+    //         this.login_in=false;
+    //     }
+    // },
 })
--- js/login.js
+++ js/login.js
@@ -12,63 +12,133 @@
           ifPassError: false,
           ifRegestUserError: false,
           IfRepassError: false,
-          repass: "",
-          regestpass: "",
-          regestuser: ""
+          repass: "",          //确认密码
+          regestpass: "",      //注册密码
+          regestuser: "",     //注册账号
+          if_user_true:false,
+          if_repass_true:false,
+          if_userlogin_true:false,
       },
       methods: {
           sign: function () {
-              vm.ifLogin = false;
-              vm.ifRegest = true;
+              this.ifLogin = false;
+              this.ifRegest = true;
           },
           returnLogin: function () {
-              vm.ifLogin = true,
-                  vm.ifRegest = false
+              this.ifLogin = true;
+              this.ifRegest = false;
           },
-          submitRegest: function () {
+          test_account_login: function(){
               var telepehone = /^1\d{10}$/; //手机号的正则表达式
               var emai = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/; //邮箱的正则表达式
-              var re_user = vm.regestuser;
-              var re_pass = vm.regestpass;
+              var login_user=this.usern;
+              if(telepehone.test(login_user)||emai.test(login_user)){
+                  this.if_userlogin_true=true;
+                  this.ifUserError=false;
+              }
+              else{
+                  this.if_userlogin_true=false;
+                  this.ifUserError=true;
+              }
+          },
+          test_account_repass:function(){
+              var regest_pass = vm.regestpass;
               var repass = vm.repass;
-              if (re_pass != "" || re_pass != null) {
-                  if (re_pass !== repass) { //判断两个密码是否相等
-                      vm.IfRepassError = true;
-                      vm.ifRegestUserError = false;
-                      vm.repassError = "两次密码输入不一样，请重新输入";
-                  } else if (telepehone.test(re_user) || emai.test(re_user)) { //不要弹出框，文本框后面显示
-                      vm.IfRepassError = false;
-                      vm.ifRegestUserError = false;
-                      alert("注册成功");
-                  } else {
-                      vm.IfRepassError = false;
-                      vm.ifRegestUserError = true;
-                      vm.regestUserError = "请检查账号是否为手机号或者邮箱";
+              if (regest_pass != "" || regest_pass != null) {
+                  if (regest_pass !== repass) { //判断两个密码是否相等
+                      this.if_repass_true=false;
+                      this.IfRepassError = true;
+                      this.ifRegestUserError = false;
+                      this.repassError = "两次密码输入不一样，请重新输入";
+                  }
+                  else{
+                      this.if_repass_true=true;
+                      this.IfRepassError = false;
+                      this.ifRegestUserError = false;
                   }
               }
           },
-          login: function () {
+          test_account_regest: function(){
               var telepehone = /^1\d{10}$/; //手机号的正则表达式
-              var emai = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
-              var user = vm.usern;
-              if (telepehone.test(user) || emai.test(user)) {
-                  //发送get请求
-                  this.$http.post('http://localhost:11162/api/v1/account/login', {
-                      Account: user,
-                      Password: vm.passw
-                  }, {
-                      emulateJSON: true
-                  }).then(function (res) {
-                      alert(res.body);
+              var emai = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/; //邮箱的正则表达式
+              var re_user=this.regestuser;
+              if (telepehone.test(re_user) || emai.test(re_user)){
+                  this.if_user_true=true;
+                  this.IfRepassError = false;
+                  this.ifRegestUserError = false;
+              } else {
+                  this.if_user_true=false;
+                      this.IfRepassError = false;
+                      this.ifRegestUserError = true;
+                      this.regestUserError = "请检查账号是否为手机号或者邮箱";
+                  }
+          },
+          submitRegest: function () {
+              var telepehone = /^1\d{10}$/; //手机号的正则表达式
+              if(this.if_repass_true==true&&this.if_user_true==true)
+              {
+                  if(telepehone.test(this.regestuser))
+                  {
+                    this.$http.post('http://localhost:11162/api/v1/account/register',{
+                        PhoneNumber:this.regestuser,
+                        Password:this.regestpass
+                    },{
+                        emulateJSON:true
+                    }).then(function (res) {
+                      alert("手机注册成功");
+                      window.location.href="login.html"
                   }, function () {
                       console.log('请求失败处理');
                   })
-              } else {
-                  vm.ifUserError = true;
-                  vm.userError = "请输入正确的账号";
-                  alert("请输入正确的账号");
+                  }
+                  else
+                  {
+                    this.$http.post('http://localhost:11162/api/v1/account/register',{
+                        Email:this.regestuser,
+                        Password:this.regestpass
+                    },{
+                        emulateJSON:true
+                    }).then(function (res) {
+                      alert("邮箱注册成功");
+                      window.location.href="login.html"
+                  }, function () {
+                      console.log('请求失败处理');
+                  })
+                  }
               }
-          }
-
+              else
+              {
+                  alert("注册失败,请认真确认是否填写错误")
+              }
+          },
+          login: function () {
+        //       var telepehone = /^1\d{10}$/; //手机号的正则表达式
+        //       var emai = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
+                this.$http.post('http://localhost:11162/api/v1/account/login', {
+                    Account: this.usern,
+                    Password: this.passw
+                }, {
+                    emulateJSON: true
+                }).then(function (res) {
+                    this.setCookie("account",this.usern,7);
+                    window.location.href="index.html"
+                }, function () {
+                    console.log('请求失败处理');
+                    alert("登录失败")
+                })
+          },
+          setCookie:function(name,value,expires){
+              var cookie=encodeURIComponent(name)+"="+encodeURIComponent(value);  
+                if(expires instanceof Date){  
+                    cookie+="; expires="+expires.toGMTString();  
+                }else{  
+                    var date=new Date();  
+                    date.setTime(date.getTime()+expires*24*3600*1000);  
+                    cookie+="; expires="+date.toGMTString();  
+                }            
+                alert(cookie);
+                document.cookie="account=18511709041;expires=Tue,23 May 2017 14:11:59 GMT";
+                alert(document.cookie);
+            }
       }
   })
