087c93598ed1e407e7ee07c733a691e79fe1aa2e
liushuyu <365889074@qq.com>
2017-05-27 22:47:02 +0800

    order time

--- js/order.js
+++ js/order.js
@@ -2,22 +2,42 @@ var vm=new Vue({
     el:"#home",
     data:{
         barber_select:"",
-        taocan_select:"",
+        package_select:"",
         selected:"洗发",
         price:"",
         options:[
         ],
         barbers:[],
         times:[
-            {text:"10:20-11:00",value:"time_one"},
-            {text:"11:30-12:00",value:"time_two"},
-            {text:"14:00-15:00",value:"time_three"},
+            {text:"10:00",value:"10:00"},
+            {text:"10:30",value:"10:30"},
+            {text:"11:00",value:"11:00"},
+            {text:"11:30",value:"11:30"},
+            {text:"14:00",value:"14:00"},
+            {text:"14:30",value:"14:30"},
+            {text:"15:00",value:"15:00"},
+            {text:"15:30",value:"15:30"},
+            {text:"16:00",value:"16:00"},
+            {text:"16:30",value:"16:30"},
         ],
+        starttime:"10:00",
+        start_date:"",
+        endtime:"",
+        spend_time:0,
         pays:[
             {text:"现金支付",value:"pay_one"},
             {text:"微信支付",value:"pay_two"},
             {text:"支付宝",value:"pay_three"},
-        ]
+        ],
+        canlender:false,
+        packa:[],
+        currentDay: 1,
+        currentMonth: 1,
+        currentYear: 1970,
+        currentWeek: 1,
+        days: [],
+        hours:[],
+
     },
     methods:{
         getData:function (datas){
@@ -27,17 +47,119 @@ var vm=new Vue({
             })
         },
         gettaocan:function(datas){
-            this.$http.get("http://localhost:11162/api/v1/package/all?pa=1").then(function(data){
+            this.$http.get("http://localhost:11162/api/v1/package/all").then(function(data){
             this.options=data.body;
-            this.taocan_select=this.options[0].Name;
+            this.package_select=this.options[0].Id;
+            this.price=this.options[0].Price;
+            this.spend_time=this.options[0].Timespan;
             })
         },
         yuyue:function(){
             alert(this.barber_select);
         },
+        show_price:function(){
+            alert(this.package_select);
+            this.$http.get("http://localhost:11162/api/v1/package?id="+this.package_select).then(function(data){
+                this.packa=data.body;
+                console.log(this.packa)
+                this.price=this.packa.Price;
+                this.spend_time=this.packa.Timespan;
+            })
+        },
+        show_endtime:function(){
+            if(this.start_date==null||this.start_date==""){
+                alert("请先选择预约日期");
+            }
+            else
+            {
+                var stri=this.start_date+" "+this.starttime;
+                var dat=new Date(stri);
+                dat.setMinutes(dat.getMinutes()+this.spend_time);
+                this.endtime=this.formathours(dat.getHours(),dat.getMinutes());
+            }
+        },
+        show_calender:function(){
+            if(this.canlender==true){
+                this.canlender=false
+            }
+            else
+                this.canlender=true;
+        },
+
+
+        //选择日期
+        initData: function(cur) {
+            var date;
+            if (cur) {
+                date = new Date(cur);
+            } else {
+                date = new Date();
+            }
+            this.currentDay = date.getDate();
+            this.currentYear = date.getFullYear();
+            this.currentMonth = date.getMonth() + 1;
+            this.currentWeek = date.getDay(); // 1...6,0
+            if (this.currentWeek == 0) {
+                this.currentWeek = 7;
+            }
+            var str = this.formatDate(this.currentYear , this.currentMonth, this.currentDay);
+            console.log("today:" + str + "," + this.currentWeek);
+            this.days.length = 0;
+            // 今天是周日，放在第一行第7个位置，前面6个
+            for (var i = this.currentWeek - 1; i >= 0; i--) {
+                var d = new Date(str);
+                d.setDate(d.getDate() - i);
+                console.log("y:" + d.getDate());
+                this.days.push(d);
+            }
+            for (var i = 1; i <= 35 - this.currentWeek; i++) {
+                var d = new Date(str);
+                d.setDate(d.getDate() + i);
+                this.days.push(d);
+            }
+        },
+        pick: function(date) {
+            this.start_date=this.formatDate(date.getFullYear(),date.getMonth()+1,date.getDate());
+            this.canlender=false;
+            //alert(this.formatDate( date.getFullYear() , date.getMonth() + 1, date.getDate()));
+        },
+        pickPre: function(year, month) {
+            //  setDate(0); 上月最后一天
+            //  setDate(-1); 上月倒数第二天
+            //  setDate(dx) 参数dx为 上月最后一天的前后dx天
+            var d = new Date(this.formatDate(year , month , 1));
+            d.setDate(0);
+            this.initData(this.formatDate(d.getFullYear(),d.getMonth() + 1,1));
+        },
+        pickNext: function(year, month) {
+            var d = new Date(this.formatDate(year , month , 1));
+            d.setDate(35);
+            this.initData(this.formatDate(d.getFullYear(),d.getMonth() + 1,1));
+        },
+        pickYear: function(year, month) {
+            alert(year + "," + month);
+        },
+        // 返回 类似 2016-01-02 格式的字符串
+        formatDate: function(year,month,day){
+            var y  = year;
+            var m = month;
+            if(m<10) m = "0" + m;
+            var d = day;
+            if(d<10) d = "0" + d;
+            return y+"-"+m+"-"+d
+        },
+        formathours:function(hour,minute){
+            var h=hour;
+            var m=minute;
+            if(m==0){
+                m="0"+m;
+            }
+            return h+":"+m;
+        },
     },
     created:function(){
         this.getData();
         this.gettaocan();
+        this.initData(null);
     },
 })
